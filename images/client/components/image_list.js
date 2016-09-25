3052cfb845953507ac30c2136c6e7a73da8c4182
qlcx <1136425019@qq.com>
2016-09-25 22:02:25 +0800

    add image_detail&image_list&main.css

---
+++ images/client/components/image_list.js
@@ -0,0 +1,26 @@
+// Create our image list component
+// Import React 
+import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'https://dummyimage.com/600x400' },
+];
+
+// Create our component
+const ImageList = () => {
+  const RenderedImages = IMAGES.map((image) => {
+    return <ImageDetail image={image} />
+  });
+
+  return (
+    <ul className="media-list list-group">
+      {RenderedImages}
+    </ul>
+  );
+}
+
+// Export our component
+export default ImageList;
