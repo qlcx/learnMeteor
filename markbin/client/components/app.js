d11a0a29cf444d93cdf98e411cf5e259251e49fa
qlcx <1136425019@qq.com>
2016-10-02 16:20:13 +0800

    add authenication UI && add Bins Method

--- markbin/client/components/app.js
+++ markbin/client/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './header';
 
 export default () => {
   return (
     <div>
-      MarkBin!
+      <Header />
     </div>
   );
 }
---
+++ markbin/imports/collections/bins.js
@@ -0,0 +1,16 @@
+import { Mongo } from 'meteor/mongo';
+
+Meteor.methods({
+  'bins.insert': function() {
+    return Bins.insert({
+      createdAt: new Date(),
+      content: '',
+      sharedWith: [],
+      // typically the id of the user record in the database
+      // docs.meteor.com/#/full/method_userid
+      ownerId: this.userId
+    });
+  }
+})
+
+export const Bins = new Mongo.Collections('bins');
